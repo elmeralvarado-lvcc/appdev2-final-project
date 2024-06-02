191f48d49012b1510e76cc4af7c22fa3bb6e4194
QIP-Elmer <qip-elmer@QIP-Elmers-MacBook-Air.local>
2024-06-02 10:47:36 +0800

    Add moneyphp/money for formatting money

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,10 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Money\Currency;
+use Money\Money;
 
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['price'];
+
+    protected function price(): Attribute
+    {
+        return Attribute::make(
+            get: function (int $value) {
+                return new Money($value, new Currency('USD'));
+            }
+        );
+    }
 
     public function variants()
     {
@@ -17,5 +31,10 @@ class Product extends Model
     public function images()
     {
         return $this->hasMany(Image::class);
+    }
+
+    public function image()
+    {
+        return $this->hasOne(Image::class)->ofMany('featured', 'max');
     }
 }
